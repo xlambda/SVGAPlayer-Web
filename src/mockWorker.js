4cbb290a0ed844faa4cd636fe22b11c1bb1f66d2
xuqianmou <xuqianmou@corp.netease.com>
2018-05-08 12:00:16 +0800

    兼容Qt4带的webkit

--- src/mockWorker.js
+++ src/mockWorker.js
@@ -8,7 +8,8 @@ const Uint8ToString = function (u8a) {
     var CHUNK_SZ = 0x8000;
     var c = [];
     for (var i = 0; i < u8a.length; i += CHUNK_SZ) {
-        c.push(String.fromCharCode.apply(null, u8a.subarray(i, i + CHUNK_SZ)));
+        var t = [].slice.call(u8a.subarray(i, i + CHUNK_SZ));
+        c.push(String.fromCharCode.apply(null, t));
     }
     return c.join("");
 }
